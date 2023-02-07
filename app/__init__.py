5281ea5f71b843f4243e97b83d1c89884d695c99
DMarrs <diegomarrs@gmail.com>
2023-02-07 10:31:04 -0800

    set up flask application

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+def create_app(test_config=None):
+    # create a new Flask object
+    # tell Flask to serve static objects from the root directory
+    app = Flask(__name__, static_url_path='/')
+    # trailing slashes are optional (i.e. /dashboard === /dashboard/)
+    app.url_map.strict_slashes = False
+    # specify a key to use for creating server-side sessions
+    app.config.from_mapping(
+        SECRET_KEY='super_secret_key'
+    )
+
+    # activate the environment and run the Flask server using python -m flask run
+    return app;
