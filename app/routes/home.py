3d93ad61c0b80f284648f774a54a279516b7be8e
DMarrs <diegomarrs@gmail.com>
2023-02-21 18:48:15 -0800

    rendered fake articles to page

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template, session, redirect
+from app.models import Article
 from app.db import get_db
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    return render_template('home.html')
+    # get all articles, sort them in descending order by creation date
+    # obtain only the first five
+    db = get_db()
+    articles = db.query(Article).order_by(Article.created_at.desc()).all()
+    articles = articles[:5]
+    return render_template('home.html', articles=articles)
