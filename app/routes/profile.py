8dc90507048f18fcf5470da7208c8de99b4fe34d
DMarrs <diegomarrs@gmail.com>
2023-04-14 21:18:05 -0700

    passed in user data from back end to front end

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, session, redirect, request
+from app.models import User
 from app.db import get_db
 
 bp = Blueprint('profile', __name__, url_prefix='/profile')
 
 @bp.route('/')
 def dashboard():
-
-    return render_template('profile.html', loggedIn=session.get('loggedIn'))
+    db = get_db()
+    user = db.query(User).filter(User.id == session.get('user_id')).one()
+    return render_template('profile.html', loggedIn=session.get('loggedIn'), user=user)
