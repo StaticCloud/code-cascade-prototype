5facc5ea3015b19bba1139227fe7caa9810754f5
DMarrs <diegomarrs@gmail.com>
2023-04-17 23:03:47 -0700

    added reactive navbar on user profile menu

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -9,6 +9,4 @@ def dashboard():
     db = get_db()
     user = db.query(User).filter(User.id == session.get('user_id')).one()
 
-    print(user.liked_articles)
-
     return render_template('profile.html', loggedIn=session.get('loggedIn'), user=user)
---
+++ app/static/javascript/profile.js
@@ -0,0 +1,20 @@
+const bioButton = document.querySelector(".bio")
+const savesButton = document.querySelector(".saves")
+const likesButton = document.querySelector(".likes")
+const commentsButton = document.querySelector(".comments")
+
+const profileNav = document.querySelector('.profile-nav')
+
+profileNav.addEventListener("click", (e) => {
+    let element = e.target;
+
+    if (element.nodeName == "IMG") {
+        element = element.parentNode
+    }
+
+    Array.from(profileNav.children).forEach(child => {
+        child.classList.remove('highlighted')
+    })
+
+    element.classList.add('highlighted')
+})
