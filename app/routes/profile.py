48a9d44004e24cacb17dc43f349809901d3ad102
DMarrs <diegomarrs@gmail.com>
2023-04-20 18:43:41 -0700

    created skeleton for update profile page

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -10,3 +10,10 @@ def dashboard():
     user = db.query(User).filter(User.id == session.get('user_id')).one()
 
     return render_template('profile.html', loggedIn=session.get('loggedIn'), user=user)
+
+@bp.route('/edit')
+def edit():
+    db = get_db()
+    user = db.query(User).filter(User.id == session.get('user_id')).one()
+
+    return render_template('edit-profile.html', loggedIn=session.get('loggedIn'), user=user)
