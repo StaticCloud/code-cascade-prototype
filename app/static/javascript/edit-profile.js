ede27714354d0f81f22c7fb9f2ffeae97afa8b2a
DMarrs <diegomarrs@gmail.com>
2023-04-30 22:54:38 -0700

    minor change to edit-profile.js

--- app/static/javascript/edit-profile.js
+++ app/static/javascript/edit-profile.js
@@ -1,3 +1,4 @@
 const newBio = document.querySelector('#bio-input')
 const newLinkedin = document.querySelector('#linkedin-input')
 const newGithub = document.querySelector('#github-input')
+const editProfileForm = document.querySelector('.profile-edit-form')
