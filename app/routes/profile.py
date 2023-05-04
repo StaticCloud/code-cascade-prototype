db139354cdc77fb32b0efe3d0165df117128b77d
DMarrs <diegomarrs@gmail.com>
2023-05-03 18:52:37 -0700

    displayed nested comments from database

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -1,15 +1,28 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, redirect
 from app.models import User
 from app.db import get_db
 
 bp = Blueprint('profile', __name__, url_prefix='/profile')
 
-@bp.route('/')
-def dashboard():
+@bp.route('/', defaults={'id': None})
+@bp.route('/<id>')
+def dashboard(id):
     db = get_db()
-    user = db.query(User).filter(User.id == session.get('user_id')).one()
+    authUser = False
 
-    return render_template('profile.html', loggedIn=session.get('loggedIn'), user=user)
+    try:
+        if id == None:
+            authUser = True
+            user = db.query(User).filter(User.id == session.get('user_id')).one()
+        else:
+            if int(id) == session.get('user_id'):
+                return redirect('/profile')
+            else:
+                user = db.query(User).filter(User.id == id).one()
+
+        return render_template('profile.html', loggedIn=session.get('loggedIn'), user=user, authUser=authUser)
+    except:
+        return redirect('/profile')
 
 @bp.route('/edit')
 def edit():
