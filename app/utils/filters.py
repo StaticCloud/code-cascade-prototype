059464a9d1fd640c6d1007f29927f5ff57278fe0
DMarrs <diegomarrs@gmail.com>
2023-05-07 19:41:53 -0700

    added form for leaving comments

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -2,4 +2,4 @@ def format_keywords(keywords):
     return ', '.join(keywords)
 
 def format_date(date):
-    return date.strftime('%m/%d/%y at %I:%M %p')
+    return date.strftime('On %m/%d/%y at %I:%M %p')
