68794c8d924fcfb7dd56a20df4e61a4327ad1bef
DMarrs <diegomarrs@gmail.com>
2023-05-16 00:35:06 -0700

    comment depth implemnted

--- app/routes/home.py
+++ app/routes/home.py
@@ -40,6 +40,12 @@ def search():
     
     return render_template('search-results.html', keywords=args.get('keywords'), articles=articles, loggedIn=session.get('loggedIn'))
     
+# calculate the comment depth
+def commentDepth(comment, depth):
+    comment.depth = depth;
+    for reply in comment.replies:
+        commentDepth(reply, depth + 1)
+
 @bp.route('/article/<id>')
 def article(id):
     db = get_db()
@@ -57,6 +63,9 @@ def article(id):
     
     article.replies = [reply for reply in article.replies if reply.parent_comment == None]
 
+    for reply in article.replies:
+        commentDepth(reply, 0)
+
     return render_template('article.html', article=article, is_liked=is_liked, is_saved=is_saved, loggedIn=session.get('loggedIn'), avatar=session.get('avatar'))
 
 @bp.route('/comment/<id>')
@@ -65,6 +74,10 @@ def comment(id):
 
     try:
         comment = db.query(Comment).where(Comment.id == id).one()
+
+        for reply in comment.replies:
+            commentDepth(reply, 1)
+
         return render_template('comment-page.html', comment=comment, loggedIn=session.get('loggedIn'), avatar=session.get('avatar'))
     except:
         return redirect('/')
